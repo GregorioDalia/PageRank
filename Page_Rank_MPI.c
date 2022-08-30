15afeab92203e005502f1c1eacd3771dba24d714
Kateryna Romanyuk <kateryna.romanyuk20@gmail.com>
2022-08-30 18:59:43 +0200

    MPI Strategy

--- Page_Rank_MPI.c
+++ Page_Rank_MPI.c
@@ -16,9 +16,10 @@ typedef struct Node
 #define ERROR 0.0001 // Real in (0, +inf), best at 0.0001
 
 int main(){
-
+  int info[2];
   // Open the data set
   //char filename[] = "./web-NotreDame.txt";
+  //if(r== 0){ // r rank del processo, 0 è il processo master
   char filename[] = "./DEMO.txt";
   printf("DEBUG: open the file %s",filename);
 
@@ -43,68 +44,152 @@ int main(){
     sscanf(str, "%*s %d %*s %d", &n, &e); // number of nodes
     ch = getc(fp);
   }
-
+  
   ungetc(ch, fp);
 
   // DEBUG: Print the number of nodes and edges, skip everything else
   printf("DEBUG:\nGraph data:\n\n  Nodes: %d, Edges: %d \n\n", n, e);
+  // Controllo numero archi: maggiore del numero di processi?
+
+  // distribuiamo ipotizzando che l'indegree dei nodi è +/- bilanciato
+  /*
+    num_nodi_per_processo_min = n/p
+    resto = n%p
+    if(resto == 0){
+      Node * sparse_matrix[num_nodi_per_processo_min]
+      for (int k = 0; k < num_nodi_per_processo_min; k++){
+        sparse_matrix[k] = NULL;
+      }
+    }
+    else{ 
+      Node * sparse_matrix[num_nodi_per_processo_min + 1]
+      for (int k = 0; k < num_nodi_per_processo_min + 1; k++){
+        sparse_matrix[k] = NULL;
+      }
+    }
+    
+  
+    info[1] = n
+    for(int i = 1; i < p; i++){
+      if(i < resto) {
+        info[0] = num_nodi_per_processo_min + 1;
+        send(i,info);
+      }
+      else{
+        info[0] = num_nodi_per_processo_min;
+        send(i,info);
+      }
+    }
+
+    
+  */
+
+
+
+  
+  //}   //Fine if master
+  /* else{
+    int info[2]
+    
+    receive(0, info)
+    Node *sparse_matrix_locale[info[0]]
+    int n = info[1]
+    // considerare uso di malloc
+
+    for (int k = 0; k < info[0]; k++){
+      sparse_matrix[k] = NULL;
+    }
+  }
+
+  */
 
   // Creation of the matrix from the file and count of outdegree and indregree of all nodes
   int fromnode, tonode;
 
-  int* in_degree = malloc(n * sizeof(int));
-  int* out_degree = malloc(n * sizeof(int));
+  //int* in_degree = malloc(n * sizeof(int)); DA CANCELLARE
+  int* out_degree = malloc(n * sizeof(int)); // Lo fanno tutti, master e workers
 
+  
   float* page_ranks = malloc(n * sizeof(float));
-  float* old_page_ranks = malloc(n * sizeof(float));
-  float* mean_coloumn_weighed = malloc(n * sizeof(float));
+  //float* old_page_ranks = malloc(n * sizeof(float)); // verione seriale
+  float* old_page_ranks = malloc(info[0] * sizeof(float)); // versione mpi
+  
+  //float* mean_coloumn_weighed = malloc(n * sizeof(float)); // verione seriale
+  float* mean_coloumn_weighed = malloc(info[0] * sizeof(float)); // versione mpi
 
   //Creation of the sparse matrix
-  Node *sparse_matrix[n];
+  //Node *sparse_matrix[n]; DA CANCELLARE
 
   printf("DEBUG: INITIALIZATION\n");
   for (int k = 0; k < n; k++){
-    in_degree[k] = 0;
+    //in_degree[k] = 0; DA CANCELLARE
     out_degree[k] = 0;
-    sparse_matrix[k] = NULL;
+    //sparse_matrix[k] = NULL; DA CANCELLARE
   }
 
+  info[0] = 0; // from node (-1 se finisce file)
+  info[1] = 0; // to node (-1 se finisce file)
+  //if (r == 0){
+    printf("\n");
+    printf("DEBUG: READ FILE\n");
 
-  printf("\n");
-  printf("DEBUG: READ FILE\n");
+    int percento = 0;
+    int m = 0;
 
-  int percento = 0;
-  int m = 0;
+    while (!feof(fp)){
+      
+      m++;
 
-  while (!feof(fp)){
-    
-    m++;
+      fscanf(fp, "%d%d", &fromnode, &tonode);
 
-    fscanf(fp, "%d%d", &fromnode, &tonode);
+      /*
+      info[0] = fromnode;
+      info[1] = tonode;
+      dest = fromnode % p // p numero processi
 
-    if(e>=100 && ((m%(e/10)) == 0)){
-        percento +=10;
-        printf("%d%% ",percento);
+      if (dest != 0){
+        send (dest, info, ...);
+      }
+      else{
+        Node *NuovoArco = (struct Node *)malloc(sizeof(Node));
+        NuovoArco->start_node = fromnode;
+        NuovoArco->end_node = tonode;
+        NuovoArco->value = 1;
+
+        // INSERIMENTO IN TESTA
+        NuovoArco->next = sparse_matrix[NuovoArco->end_node];
+        sparse_matrix[NuovoArco->end_node] = NuovoArco;
+      }
+      */
+
+      if(e>=100 && ((m%(e/10)) == 0)){
+          percento +=10;
+          printf("%d%% ",percento);
+      }
+
+      Node *NuovoArco = (struct Node *)malloc(sizeof(Node));
+      NuovoArco->start_node = fromnode;
+      NuovoArco->end_node = tonode;
+      NuovoArco->value = 1;
+
+
+      // INSERIMENTO IN TESTA
+      NuovoArco->next = sparse_matrix[NuovoArco->end_node];
+      sparse_matrix[NuovoArco->end_node] = NuovoArco;
+
+      // use fromnode and tonode as index
+      out_degree[fromnode]++;
+      //in_degree[tonode]++; DA CANCELLARE
+
     }
-
-    Node *NuovoArco = (struct Node *)malloc(sizeof(Node));
-    NuovoArco->start_node = fromnode;
-    NuovoArco->end_node = tonode;
-    NuovoArco->value = 1;
-
-
-    // INSERIMENTO IN TESTA
-    NuovoArco->next = sparse_matrix[NuovoArco->end_node];
-    sparse_matrix[NuovoArco->end_node] = NuovoArco;
-
-    // use fromnode and tonode as index
-    out_degree[fromnode]++;
-    in_degree[tonode]++;
-
-  }
+  //} fine if master
   printf("\n");
 
-
+  /*
+    DISTRIBUZIONE ARCHI ROUND ROBIN, USANDO MODULO;
+    MOLTIPLICAZIONE RIGA PER COLONNA FATTA DA OGNI WORKER, CHE NON COSTRUISCE UN ARRAY MA UNA MATRICE (chiamata my_page_ranks),
+    DA INVIARE AL MASTER: [POSIZIONE IN page_ranks COMPLETO, VALORE]
+  */
   printf("DEBUG: INITIALIZE PAGE RANKS TO 1/N\n");
   
   percento = 0;
