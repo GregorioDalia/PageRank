fe8a7679f9942702fe15e78c14c4965158659d48
Kateryna Romanyuk <kateryna.romanyuk20@gmail.com>
2022-08-31 12:28:31 +0200

    MPI strategy, follow up
    Co-authored-by: Stefano Rondinella <Smaug73@users.noreply.github.com>

--- Page_Rank_MPI.c
+++ Page_Rank_MPI.c
@@ -56,18 +56,16 @@ int main(){
     num_nodi_per_processo_min = n/p
     resto = n%p
     if(resto == 0){
-      Node * sparse_matrix[num_nodi_per_processo_min]
-      for (int k = 0; k < num_nodi_per_processo_min; k++){
-        sparse_matrix[k] = NULL;
-      }
+      size = num_nodi_per_processo_min;
     }
     else{ 
-      Node * sparse_matrix[num_nodi_per_processo_min + 1]
-      for (int k = 0; k < num_nodi_per_processo_min + 1; k++){
-        sparse_matrix[k] = NULL;
-      }
+      size = num_nodi_per_processo_min + 1;
     }
-    
+
+    Node * sparse_matrix[size]
+    for (int k = 0; k < size; k++){
+        sparse_matrix[k] = NULL;
+    }
   
     info[1] = n
     for(int i = 1; i < p; i++){
@@ -92,11 +90,12 @@ int main(){
     int info[2]
     
     receive(0, info)
-    Node *sparse_matrix_locale[info[0]]
+    size = info[0]
+    Node *sparse_matrix_locale[size] // considerare uso di malloc
     int n = info[1]
-    // considerare uso di malloc
+    
 
-    for (int k = 0; k < info[0]; k++){
+    for (int k = 0; k < size; k++){
       sparse_matrix[k] = NULL;
     }
   }
@@ -129,16 +128,14 @@ int main(){
 
   info[0] = 0; // from node (-1 se finisce file)
   info[1] = 0; // to node (-1 se finisce file)
+  
   //if (r == 0){
     printf("\n");
     printf("DEBUG: READ FILE\n");
 
-    int percento = 0;
-    int m = 0;
 
     while (!feof(fp)){
       
-      m++;
 
       fscanf(fp, "%d%d", &fromnode, &tonode);
 
@@ -157,32 +154,64 @@ int main(){
         NuovoArco->value = 1;
 
         // INSERIMENTO IN TESTA
-        NuovoArco->next = sparse_matrix[NuovoArco->end_node];
-        sparse_matrix[NuovoArco->end_node] = NuovoArco;
+        NuovoArco->next = sparse_matrix[NuovoArco->end_node % size];
+        sparse_matrix[NuovoArco->end_node % size] = NuovoArco;
       }
       */
-
-      if(e>=100 && ((m%(e/10)) == 0)){
-          percento +=10;
-          printf("%d%% ",percento);
-      }
-
-      Node *NuovoArco = (struct Node *)malloc(sizeof(Node));
-      NuovoArco->start_node = fromnode;
-      NuovoArco->end_node = tonode;
-      NuovoArco->value = 1;
-
-
-      // INSERIMENTO IN TESTA
-      NuovoArco->next = sparse_matrix[NuovoArco->end_node];
-      sparse_matrix[NuovoArco->end_node] = NuovoArco;
 
       // use fromnode and tonode as index
       out_degree[fromnode]++;
       //in_degree[tonode]++; DA CANCELLARE
 
     }
+    /*
+    // Send the edges
+    info[0] = -1;
+    info[1] = -1;
+    for(i = 1; i < p; i++){
+      send(i, info);
+      MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest,
+        int tag, MPI_Comm comm)
+    }
+
+    // Mettere barrier?
+    // Send the out_degree array
+    for(i = 1; i < p; i++){
+      send(i, out_degree);
+      MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest,
+        int tag, MPI_Comm comm)
+    }
+    */
   //} fine if master
+  /* else{  // WORKER receive edge code
+      
+      // Receive the new edge
+      receive(0, info)
+      MPI_Recv(void *info, int count, MPI_Datatype datatype,
+          int source, int tag, MPI_Comm comm, MPI_Status *status)
+
+      // Check if the master doesn't reach the eof
+      while(info[0] != -1 && info[1] != -1){
+        
+        Node *NuovoArco = (struct Node *)malloc(sizeof(Node));
+        NuovoArco->start_node = fromnode;
+        NuovoArco->end_node = tonode;
+        NuovoArco->value = 1;
+
+
+        // INSERIMENTO IN TESTA
+        NuovoArco->next = sparse_matrix[NuovoArco->end_node % size];
+        sparse_matrix[NuovoArco->end_node % size] = NuovoArco;
+
+        // Receive the new edge
+        receive(0, info)
+        MPI_Recv(void *info, int count, MPI_Datatype datatype,
+          int source, int tag, MPI_Comm comm, MPI_Status *status)
+
+      }
+
+      receive(0, out_degree);
+  }*/
   printf("\n");
 
   /*
@@ -190,47 +219,38 @@ int main(){
     MOLTIPLICAZIONE RIGA PER COLONNA FATTA DA OGNI WORKER, CHE NON COSTRUISCE UN ARRAY MA UNA MATRICE (chiamata my_page_ranks),
     DA INVIARE AL MASTER: [POSIZIONE IN page_ranks COMPLETO, VALORE]
   */
-  printf("DEBUG: INITIALIZE PAGE RANKS TO 1/N\n");
-  
-  percento = 0;
-  m=0;
+
+
+
+
+  printf("DEBUG: INITIALIZE PAGE RANKS TO 1/N and UPDATE MATRIX\n");
+
+  // Same code MASTER and WORKER
   for (int i = 0; i < n; i++){
-    
-    m++;
-    page_ranks[i] = 1.0 / (float)n;
+
+  // old_page_rank è il page rank completo, ricevuto dal master, new_page_ranks è invece
+  // il sotto-array di page rank calcolato dal worker e ha dimensione size.
+  // Per fare la differenza, il worker accede in old_page_ranks a partire dal proprio rank(nel senso proprio id) e saltando di p
+    if(i<size){
+      new_page_ranks[i] = 1.0 / (float)n;
+      mean_coloumn_weighed[i] = (1 - WEIGHT) / (float)n;
+      
+      Node *pointer = sparse_matrix[i];
+
+      // Update the value of the pointer
+      while (pointer != NULL){  
+        pointer->value = (WEIGHT / (float)out_degree[pointer->start_node]);
+        pointer = pointer->next;
+      }
+      
+    }
+      
+
     old_page_ranks[i] = 1.0 / (float)n;
-    mean_coloumn_weighed[i] = (1 - WEIGHT) / (float)n;
-
-    if(n>=100 && ((m%(n/10)) == 0)){
-        percento +=10;
-        printf("%d%% ",percento);
-    }
-
+    
   }
-
-  printf("\nDEBUG: UPDATE MATRIX\n");
   
-  percento = 0;
-  m=0;
-  
-  for (int i = 0; i < n; i++){
-    
-    m++;
-    
-    Node *pointer = sparse_matrix[i];
-
-    if(n>=100 && ((m%(n/10)) == 0)){
-        percento +=10;
-        printf("%d%% ",percento);
-    }
-
-    // Update the value of the pointer
-    while (pointer != NULL){  
-      pointer->value = (WEIGHT / (float)out_degree[pointer->start_node]);
-      pointer = pointer->next;
-    }
-
-  }    
+     
   
   printf("\n");
 
@@ -241,17 +261,9 @@ int main(){
   do{
     printf("\nDEBUG: GIRO N: %d\n", count + 1);
     score_norm = 0;
-
-    percento = 0;
-    m=0;
+  
     for (int i = 0; i < n; i++){
 
-      m++;
-      
-      if(n>=100 && ((m%(n/10)) == 0)){
-          percento +=10;
-          printf("%d%% ",percento);
-      }
 
       float sum = 0.0;
       Node *currNode = sparse_matrix[i];
@@ -290,10 +302,33 @@ int main(){
   printf("DEBUG: NUMBER OF ITERATION: %d\n", count);
   printf("\n");
 
+  /*
+    int x,k,i,newElem;
+    //x indice risultati locali
+    //k ID processo da cui ricevere
+    //newElem buffer di ricezione di dimensione 1
+    
+    // Il processo master non ha bisogno di un local_page_ranks ma inserire i valori, man mano che li calcola,
+    // direttamente in page_ranks, saltando ogni volta di p
+    
+    for (i=0,x=0,k=0;i<n;i++){
+        if(k != 0){
+          receive(k,newElem , int ...)
+          page_rank[i]=newElem;
+        }
+        k++;
+        if (k==p)k=0;
+
+    }
+
+
+    */
 
   for (int i = 0; i < n; i++){
     printf("THE PAGE RANKE OF NODE %d IS : %0.15f \n", i , page_ranks[i]);
   }
+
+  
 
   return 0;
 }
