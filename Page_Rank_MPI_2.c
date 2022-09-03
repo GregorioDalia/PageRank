9a93445b26b53710980bb830461fbf216968edfd
GregorioDalia <gregoriodalia@gmail.com>
2022-09-03 20:05:41 +0200

    Node *

--- Page_Rank_MPI_2.c
+++ Page_Rank_MPI_2.c
@@ -174,7 +174,7 @@ int main(int argc, char *argv[]){
 
       printf("DEBUG: MASTER HAS %d ROW\n",rows_num);
 
-      sparse_matrix_local = malloc(rows_num * sizeof(Node ));
+      sparse_matrix_local = malloc(rows_num * sizeof(Node *));
       for (int k = 0; k < rows_num; k++){
           sparse_matrix_local[k] = NULL;
       }
@@ -190,7 +190,7 @@ int main(int argc, char *argv[]){
                     &status);
     rows_num = info[0];
 
-    sparse_matrix_local = malloc(rows_num * sizeof(Node )); 
+    sparse_matrix_local = malloc(rows_num * sizeof(Node *)); 
     
     n = info[1];
     
