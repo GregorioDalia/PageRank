e7418da327b58d83c0a6bb70325c269671abb8ac
GregorioDalia <gregoriodalia@gmail.com>
2022-09-03 15:24:43 +0200

    old mpi

--- Page_Rank_MPI.c
+++ Page_Rank_MPI.c
@@ -30,8 +30,8 @@ int main(int argc, char *argv[]){
 
   // Variables for input file reading
   //char filename[] = "./DEMO.txt";    /* file containing the list of the edges */
-  char filename[] = "./web-NotreDame.txt";    /* file containing the list of the edges */
-  //char filename[] = "./DEMO.txt";       /* file containing the list of the edges */
+  //char filename[] = "./web-NotreDame.txt";    /* file containing the list of the edges */
+  char filename[] = "./DEMO.txt";       /* file containing the list of the edges */
   FILE *fp;                             /* file pointer */
   char ch;                              /* reads the characters in the file */
   char str[100];                        /* buffer for storing file lines */
