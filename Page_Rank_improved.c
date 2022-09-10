da93b8db256410ecc61f6794a106eda94251c6fa
GregorioDalia <gregoriodalia@gmail.com>
2022-09-10 12:16:34 +0200

    fix 5

--- Page_Rank_improved.c
+++ Page_Rank_improved.c
@@ -397,7 +397,7 @@ int main(int argc, char *argv[]){
       currNode = sparse_matrix_local[i];
 
       while (currNode!=NULL){
-        sum += (old_page_ranks[currNode->start_node] * currNode->value);
+        sum += (old_complete_page_ranks[currNode->start_node] * currNode->value);
         currNode = currNode->next;
       } 
 
